edb6b5373491f9c2413e1326418a2014d9f7672b
vini-jose <J21990938574@gmail.com>
2023-08-07 21:34:44 -0300

    Modificação na pergunta

--- aula_11/ex83.py
+++ aula_11/ex83.py
@@ -1,4 +1,4 @@
-#2. Crie uma função que receba dois números como parametro e retorne a soma deles. Por exemplo, se a função for chamada de adicionar,
+# 2. Crie uma função que receba dois números como parametro e retorne a soma deles. Por exemplo, se a função for chamada de adicionar,
 # chamar adicionar(1,2) deve retornar 3. 
 
 def adicionar():
--- aula_11/ex84.py
+++ aula_11/ex84.py
@@ -1,4 +1,4 @@
-#3. Crie uma função que receba uma string como parametro e retorne a string invertida. Por exemplo, se a função for chamada de
+# 3. Crie uma função que receba uma string como parametro e retorne a string invertida. Por exemplo, se a função for chamada de
 #inverter_string, chamar inverter_string("olá") deve retornar "álO"
 
 def inverter_palavra():
