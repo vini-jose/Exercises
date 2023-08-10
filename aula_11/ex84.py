6d8da70e13ae95aaa96a1f8f35f61899aa3e0640
vini-jose <assisjose481@gmail.com>
2023-08-10 15:19:29 -0300

    teste

--- aula_11/ex84.py
+++ aula_11/ex84.py
@@ -1,10 +1,9 @@
 # 3. Crie uma função que receba uma string como parametro e retorne a string invertida. Por exemplo, se a função for chamada de
 #inverter_string, chamar inverter_string("olá") deve retornar "álO"
 
-def get_palavra():
+def palavra():
     palavra = input("Digite seu nome: ")
-    palavra_invertida = palavra[::-1]
-    print(palavra_invertida)
-    return palavra_invertida
+    palavra_invertida = palavra[::-1] 
+    return print(palavra_invertida)
 
-get_palavra()
+palavra()
