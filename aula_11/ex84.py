11040572756692d2b02df5e36a470a4ca4cd02e1
vini-jose <j21990938574@gmail.com>
2023-08-10 21:50:30 -0300

    Mudando a def do ex84 para get_palavra

--- aula_11/ex84.py
+++ aula_11/ex84.py
@@ -1,9 +1,9 @@
 # 3. Crie uma função que receba uma string como parametro e retorne a string invertida. Por exemplo, se a função for chamada de
 #inverter_string, chamar inverter_string("olá") deve retornar "álO"
 
-def palavra():
+def get_palavra():
     palavra = input("Digite seu nome: ")
     palavra_invertida = palavra[::-1] 
     return print(palavra_invertida)
 
-palavra()
+get_palavra()
