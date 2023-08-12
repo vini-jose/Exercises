5f7b9310525f94a262cc80ba91490ac0c3765993
vini-jose <j21990938574@gmail.com>
2023-08-12 17:21:08 -0300

    Fazendo ex59

--- aula_07/ex59.py
+++ aula_07/ex59.py
@@ -3,3 +3,12 @@
 # A função deve retornar True se a palavra for um palíndromo e False caso
 # contrário.
 
+def verifica_palindromo(palavra):
+    palavra_invertida = palavra[::-1]
+    
+    if palavra_invertida == palavra:
+        return print("True")        
+    else:
+        return print("False")
+    
+verifica_palindromo("ata")
