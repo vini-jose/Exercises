0924ea4ed9e78c75527c7dfce997bc21c99310b6
vini-jose <j21990938574@gmail.com>
2023-08-12 17:29:36 -0300

    Fazendo a atividade 61

--- aula_08/ex61.py
+++ aula_08/ex61.py
@@ -4,19 +4,15 @@
 # usuário inserir um valor não numérico
     
 def main():
-    n = get_int()
-    dobro = n*2
-    print(f"O número é {n} e o dobro dele é {dobro}")
-
-
-
-
-def get_int():
     while True:
         try:
-            return int(input("Digite um número: "))        
+            numero = int(input("Digite um número: "))
+            dobro = numero*2
+            print(f"O número é {numero} e o dobro dele é {dobro}")
+            return numero
         except ValueError:
             print("Valor invalido")
+            break
         
 main()        
                  
