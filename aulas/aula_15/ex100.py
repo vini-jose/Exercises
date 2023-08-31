fb0fb7a4b947cf32e973867e58de29f514be5c04
vini-jose <assisjose481@gmail.com>
2023-08-31 17:07:26 -0300

    salvando atividade 100

--- aulas/aula_15/ex100.py
+++ aulas/aula_15/ex100.py
@@ -1,3 +1,11 @@
 # Desafio
 # Crie uma classe VerificaCPF que possui um método da classe que 
 # verifica e valida um cpf retornando True caso seja um CPF válido e False para um CPF inválido
+
+class VerificaCPF:
+    def __init__(self, cpf) -> None:
+        self.cpf = cpf
+
+    @classmethod
+    def get(cls):
+        cpf = float
