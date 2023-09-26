7a971edcca6dfbc43968fb0dff13c9410beaece0
vini-jose <assisjose481@gmail.com>
2023-09-26 17:08:21 -0300

    Desafio do abelha

---
+++ aulas/aula_16/ex101.py
@@ -0,0 +1,16 @@
+# Seletor de aluno 
+# 1) Escreva um programa que permita selecionar um aluno aleatório de um arquivo txt
+# 2) Permita que o porgrama adicione um aluno a lista
+# 3) Permita que o programa remova um aluno a lista
+
+lista = open("aluno.txt", "r")
+ler = lista.read()
+
+def main(): 
+    print("Lista de Alunos")
+
+def adicionar():
+    novo_aluno = input("Adicione um novo aluno: ")
+    ler
+
+main()
