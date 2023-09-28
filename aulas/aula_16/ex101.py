1c7ad540c126b1820629dd9ec259cd4a6a9abac3
vini-jose <assisjose481@gmail.com>
2023-09-28 17:00:28 -0300

    desafio do abelha

--- aulas/aula_16/ex101.py
+++ aulas/aula_16/ex101.py
@@ -3,14 +3,27 @@
 # 2) Permita que o porgrama adicione um aluno a lista
 # 3) Permita que o programa remova um aluno a lista
 
-lista = open("aluno.txt", "r")
-ler = lista.read()
+import random
 
-def main(): 
-    print("Lista de Alunos")
+def selecionar_aluno():
+    with open("aluno.txt", "r") as file:
+        aleatorio = file.readlines()
+        print(random.choice(aleatorio).strip())
 
-def adicionar():
-    novo_aluno = input("Adicione um novo aluno: ")
-    ler
+def adicionar_aluno():
+    novo_aluno = input("Digite o nome de um aluno: ")
+    with open("aluno.txt", "a") as file:
+        file.write("\n"+novo_aluno.strip())
 
-main()
+def remover_aluno():
+    with open("aluno.txt", "r") as file:
+        texto = file.read()
+        palavra = texto.split("\n")
+        
+    remover = int(input("Digite o numero para remover o aluno: "))
+    del palavra[remover]
+
+   
+
+
+remover_aluno()
