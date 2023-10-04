0484be29f3067fc956b258bf2a8428a11809fef6
vini-jose <assisjose481@gmail.com>
2023-10-04 17:08:48 -0300

    Atividade do ex 101 feita

--- aulas/aula_16/ex101.py
+++ aulas/aula_16/ex101.py
@@ -5,25 +5,40 @@
 
 import random
 
+def mostrando_a_lista():
+    with open("aluno.txt","r") as file:
+        print("-"*30)
+        print(f"Abrindo o arquivo:\n{file.read()}")
+
 def selecionar_aluno():
     with open("aluno.txt", "r") as file:
         aleatorio = file.readlines()
-        print(random.choice(aleatorio).strip())
+        print("-"*30)
+        print(f"O aluno selecionado é: {random.choice(aleatorio)}".strip())
 
 def adicionar_aluno():
+    print("-"*30)
     novo_aluno = input("Digite o nome de um aluno: ")
     with open("aluno.txt", "a") as file:
         file.write("\n"+novo_aluno.strip())
+    
+    print("-"*30)
+    file_atualizada = open("aluno.txt","r")
+    print(f"Lista atualizada:\n{file_atualizada.read()}")
 
 def remover_aluno():
     with open("aluno.txt", "r") as file:
         texto = file.read()
         palavra = texto.split("\n")
         
+    print("-"*30)
     remover = int(input("Digite o numero para remover o aluno: "))
     del palavra[remover]
 
-   
+    print("-"*30)
+    print(f"Lista atualizada:\n{palavra}")
 
-
+mostrando_a_lista()
+selecionar_aluno()
+adicionar_aluno()
 remover_aluno()
