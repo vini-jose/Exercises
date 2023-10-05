8de496f9c63e60473499999339cf85416c643a77
vini-jose <assisjose481@gmail.com>
2023-10-05 17:01:25 -0300

    Começando a fazer a tela com o PySimpleGUI

--- aulas/aula_16/ex102.py
+++ aulas/aula_16/ex102.py
@@ -1,1 +1,15 @@
 # Desafio: Fazer um tela com o arquivo do aluno.txt
+#-PysimpleGUI ou Tkinter
+
+import PySimpleGUI as sg
+
+sg.theme("Dark Grey 13")
+
+layout = [[sg.Text("Filename")],
+          [sg.Input(), sg.FileBrowse()],
+          [sg.OK(), sg.Cancel()]]
+
+window = sg.Window("Get filename  example", layout)
+
+event, values = window.read()
+window.close()
